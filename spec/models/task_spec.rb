d380463ba218666b08a5affd23aaf92bb4c12e48
peketo231 <peketo231@gmail.com>
2021-02-02 23:46:04 +0900

    Fix: unique制約のカラムにsequenceを使用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,28 +3,35 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      expect(build(:task)).to be_valid
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
-      task_without_title = FactoryBot.build(:task, title: nil)
-      expect(task.errors[:title]).to be_invalid include("can't be blank")
+      task_without_title = build(:task, title: "")
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'is invalid without status' do
-      task_without_status = FactoryBot.build(:task, status: nil)
-      expect(task.errors[:status]).to be_invalid include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to eq ["can't be blank"]
     end
 
     it 'is invalid with a duplicate title' do
-      FactoryBot.create(:task)
-      task_with_duplicated_title = FactoryBot.build(:task)
-      expect(task.errors[:title]).to be_invalid include("has already been taken")
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
     end
 
     it 'is valid with another title' do
-      create(:task)
-      expect(build(:task, title: "title02")).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
